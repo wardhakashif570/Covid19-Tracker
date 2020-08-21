c1f93714fc5dd40362b7c60a8370e3d4d97cb7f9
wardhakashif <wardhakashif570@gmail.com>
2020-08-21 16:42:24 -0700

    fetch global api

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import NavBar from './Components/NavBar.js';
+import InfoPanel from './Components/InfoPanel.js';
+
+function App() {
+  return (
+    <div>
+     <NavBar/>
+     <InfoPanel/>
+    </div>
+  );
+}
+
+export default App;
